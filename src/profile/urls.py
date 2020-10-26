a801e37706047a5465bf3cd3d6a4acf5b8368557
azat.zakirov <zakirmalay@gmail.com>
2020-10-26 22:28:32 +0500

    feat(profile): отключена возможность регистрации через UI

--- src/profile/urls.py
+++ src/profile/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url('^login/$', views.LoginView.as_view(), name='login'),
     url('^logout/$', views.logout, name='logout'),
-    url('^signup/$', views.SignUpView.as_view(), name='signup'),
+    # url('^signup/$', views.SignUpView.as_view(), name='signup'),
 ]
